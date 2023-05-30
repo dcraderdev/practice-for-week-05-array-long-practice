aba6cf072f9dd98df9dc2cca07eb12cda7f2fbc9
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-05-29 19:45:39 -0700

    array practice

--- array-practice.js
+++ array-practice.js
@@ -81,6 +81,28 @@ const twoSum = (arr, target) => {
 
 
 
+// const secondLargest = arr => {
+//   if(arr.length<=1){
+//    return undefined;
+//   }
+
+// 	let firstL = Math.max(...arr);
+//   let indexing = arr.indexOf(firstL);
+
+//    arr.splice(indexing,1);
+
+//    return Math.max(...arr);
+//   };
+
+//   // Time complexity = O(n^2)
+
+
+//   const arr = [4, 2, 3, 6, 8, 11, 13, 16, 18, 55, 65, 64, 47 ,56, 36, 25, 27, 29, 44, 145, 1235,1235,1235,1235,1235,46,257,56,845,785,72,1456,1234,67,368,36,8245,613,46,235,73,4578,35,62,56,234,562,367,234,623,462,34,56234,6,457,36,834,567,2346,2346,4235,834,57,24,6];
+
+// console.log(secondLargest(arr));
+
+
+
 const secondLargest = arr => {
   if(arr.length<=1){
    return undefined;
@@ -94,11 +116,10 @@ const secondLargest = arr => {
    return Math.max(...arr);
   };
 
-  // Time complexity = O(n^2)
+  // Time complexity = O(n)
 
 
-
-
+  
 const shuffle = (arr) => {
   let newArr = [...arr]
   for (let i = 0; i < arr.length; i++) {
